a4399c8952c25411dfa3ccf90ff258c96c4a90b6
nabih-softic <73518427+Muhammed-Nabih@users.noreply.github.com>
2024-12-22 01:44:22 +0200

    Ordering Infrastucture Layer

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +12,11 @@ builder.Services
     .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
-
+app.UseApiServices();
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
